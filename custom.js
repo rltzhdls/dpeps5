824d88c9817f7ddfcd955e9e05cbc6f9c64441b3
rltzhdls <136443821+rltzhdls@users.noreply.github.com>
2024-10-02 17:11:50 +0900

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,5 @@
+ $(document).ready(function () {
+        var vds = document.getElementById('vds');
+        vds.src = "https://cdn.jsdelivr.net/gh/rltzhdls/dpeps3@main/Lineage.mp4";
+        document.getElementById('vd').load();
+    });
